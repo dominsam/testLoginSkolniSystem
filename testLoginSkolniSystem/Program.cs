1813bcdf5755918b0003ef3be54188e70b8f1dd1
dsamsenak <dsamsenak@D251-11>
2025-11-07 11:12:06 +0100

    Přidejte soubory projektu.

---
+++ testLoginSkolniSystem/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace testLoginSkolniSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            int userType;
+
+            Console.WriteLine("Dobrý den, zadejte prosím váš Username!");
+            userName = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Zadejte prosím váš typ účtu.\n----------------\n1 = Žák\n2 = Učitel");
+                string accountType = Console.ReadLine();
+
+                if (accountType == "1")
+                {
+                    userType = 1;
+                    break;
+                }
+                else if (accountType == "2")
+                {
+                    userType = 2;
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Zadal jste špatný typ uživatelského účtu!");
+                }
+
+
+            }
+
+            if (userType == 1)
+            {
+                Console.WriteLine("Vítejte žáku, " + userName);
+            }
+            else
+            {
+                 Console.WriteLine("Vítejte učiteli, " + userName);
+            }
+        }
+    }
+}
